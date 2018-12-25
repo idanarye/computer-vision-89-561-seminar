19dc3bfc2bc9d5f1f83c1bc4f4a98f963d792553
IdanArye <idanarye@gmail.com>
2018-12-26 00:57:04 +0200

    Fix prepareForTouchPointAngleVertexSequence

--- prepareForTouchPointAngleVertexSequence.m
+++ prepareForTouchPointAngleVertexSequence.m
@@ -16,31 +16,37 @@ writeIm('outline', handle2image(f));
 
 % polygonCenter = getMainCentroid(im, 'Centroid').Centroid;
 circle = findMBC(fliplr(poly));
-circleCenter = circle(1:2);
+center = circle(1:2);
+center = flip(polygonCenter);
 % drawCircle(circle);
 thetas = [];
 prevTheta = [];
+veclens = [];
 for vertex = poly'
 	vertex = vertex';
-	vec = vertex - circleCenter;
+	vec = vertex - center;
+	veclens = [veclens; norm(vec)];
 	theta = atan2(vec(1), vec(2));
 	thetas = [thetas; theta];
-	drawPolyline(fliplr([circleCenter; vertex]), 'color', 'red', 'linewidth', 2);
+	drawPolyline(fliplr([center; vertex]), 'color', 'red', 'linewidth', 2);
 end
-sort(thetas);
+thetas = sort(thetas);
 thetas = [thetas; thetas(1)];
 angles = [];
 for i = 1:(size(thetas) - 1)
 	t1 = thetas(i);
 	t2 = thetas(i + 1);
-	if t2 < t1
+	while t1 < 0
+		t1 = t1 + 2 * pi;
+	end
+	while t2 < t1
 		t2 = t2 + 2 * pi;
 	end
 
 	angle = t2 - t1;
 	angles = [angles angle];
 
-	drawCircleArc(circleCenter(2), circleCenter(1), circle(3) * (0.1 + 0.03 * (t2 - t1)), rad2deg(t1), rad2deg(angle), 'color', 'green');
+	drawCircleArc(center(2), center(1), max(veclens) * (0.1 + 0.06 * (t2 - t1)), rad2deg(t1), rad2deg(angle));
 end
 writeIm('angles', handle2image(f));
 
@@ -48,7 +54,8 @@ clf;
 bar(angles, 0.1, 'facecolor', 'red');
 writeIm('anglesHist', handle2image(f));
 clf;
-bar(0:size(angles) - 1, fft(angles, 10), 0.1, 'facecolor', 'yellow');
+anglesFourier = abs(fft(angles, 10));
+bar(0:(length(anglesFourier) - 1), anglesFourier, 0.1, 'facecolor', 'yellow');
 writeIm('anglesFourier', handle2image(f));
 hold off
 end
