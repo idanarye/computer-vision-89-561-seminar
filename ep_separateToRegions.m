bbf0eb425277f58b91c5e2a33b8be735823bc3a7
IdanArye <idanarye@gmail.com>
2018-12-28 20:41:17 +0200

    Nothing important

--- ep_separateToRegions.m
+++ ep_separateToRegions.m
@@ -1,7 +1,8 @@
 pkg load image;
 pkg load geometry;
 
-im = imread('tiger2.jpeg');
+% im = imread('tiger2.jpeg');
+im = imread('log1.jpeg');
 
 % method = 'Otsu';
 % method = 'concavity';
@@ -20,14 +21,19 @@ im = imread('tiger2.jpeg');
 % edge(rgb2gray(im), 'Lindeberg', 2);
 % imshow(im2bw(im, 'minimum'))
 % imshow(im)
-% imshow(rgb2gray(im) < 255)
+% imshow(rgb2gray(im) < 254)
+% imwrite(rgb2gray(im) < 254, 'logShape.png')
 % rhist = imhist(im(:, :, 1));
 % ghist = imhist(im(:, :, 2));
 % bhist = imhist(im(:, :, 3));
 % imhist(rgb2hsv(im)(:, :, 1))
+size(im)
+texturePart = ((im(70:140, 380:450, :)));
+imshow(texturePart)
+imwrite(rgb2gray(texturePart), 'texture.png')
 % imwrite((rgb2gray(im(250:300, 250:300, :))), 'texture.png')
 % [ind, map] = rgb2ind(im);
 % imhist(ind, map)
-imHsv = rgb2hsv(im);
-imhist(imHsv(:, :, 1))
-colormap(summer(64))
+% imHsv = rgb2hsv(im);
+% imhist(imHsv(:, :, 1))
+% colormap(summer(64))
